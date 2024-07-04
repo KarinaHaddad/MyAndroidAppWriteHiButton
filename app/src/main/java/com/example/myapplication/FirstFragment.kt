2620ef3297995faf25155373a8e2703fd0561600
Karina Haddad <kahaddads@gmail.com>
2024-07-04 09:15:50 -0300

    Hi buttos

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+private var _binding: FragmentFirstBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+      _binding = FragmentFirstBinding.inflate(inflater, container, false)
+      return binding.root
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        binding.myName.setOnClickListener{
+            val nome = binding.textInputEditText3.text.toString()
+            binding.textView2.text = "Hi $nome"
+        }
+    }
+
+override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
